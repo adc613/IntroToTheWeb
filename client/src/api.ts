50c3497830495a1b1cf3594c123ad4d06c5b6cc5
Adam Collins <me@adamcollins.io>
2021-03-07 18:42:45 +0000

    Add support for creating post

--- client/src/api.ts
+++ client/src/api.ts
@@ -4,12 +4,34 @@ export interface Post {
   uid: number;
 }
 
-export function getPost(): Promise<Post[]> {
-  return Promise.resolve([
+export function getPosts(): Promise<Post[]> {
+  return simulateNetwork(posts);
+}
+
+export function createPost(content: string, author: string): Promise<Post[]> {
+  addPostToPostsArray(content, author);
+  return getPosts();
+}
+
+function simulateNetwork<T>(data: T): Promise<T> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data);
+    }, Math.random() * 2000);
+  });
+}
+
+let posts: Post[] = [
     {uid: 1, content: 'content 1', author: "Captain Jack Sparrow"},
     {uid: 2, content: 'content 2', author: "Captain Jack Sparrow"},
     {uid: 3, content: 'content 3', author: "Captain Jack Sparrow"},
     {uid: 4, content: 'content 4', author: "Captain Jack Sparrow"},
     {uid: 5, content: 'content 5', author: "Captain Jack Sparrow"},
-  ]);
+];
+
+let nextUid = 0;
+
+function addPostToPostsArray(content: string, author: string) {
+  posts.push({content: content, author: author, uid: nextUid});
+  nextUid = nextUid + 1;
 }
--- client/src/feed/Feed.tsx
+++ client/src/feed/Feed.tsx
@@ -1,5 +1,5 @@
-import {Component} from 'react';
-import {Post, getPost} from '../api';
+import {FormEvent, Component, ChangeEvent} from 'react';
+import {Post, getPosts, createPost} from '../api';
 
 interface FeedState {
   isLoading: boolean;
@@ -13,11 +13,24 @@ export class Feed extends Component<{}, FeedState> {
       isLoading: true,
       posts: [],
     };
-    getPost().then(posts => {
+    getPosts().then(posts => {
       this.setState({
         ...this.state,
         isLoading: false,
         posts: posts,
+      });
+    });
+  }
+
+  createPostHandler(content: string, author: string) {
+    this.setState({
+      isLoading: true,
+      posts: this.state.posts,
+    });
+    createPost(content, author).then((newPosts: Post[]) => {
+      this.setState({
+        isLoading: false,
+        posts: newPosts,
       });
     });
   }
@@ -41,9 +54,16 @@ export class Feed extends Component<{}, FeedState> {
     });
 
     return (
-      <ul>
-        {posts}
-      </ul>
+      <div>
+        <ul>
+          {posts}
+        </ul>
+        <PostForm
+          createPostHandler={
+            (content: string, author: string) => this.createPostHandler(content, author)}
+        >
+        </PostForm>
+      </div>
     );
   }
 
@@ -65,5 +85,76 @@ class PostComponent extends Component<PostComponentProps, {}> {
         <h2>{this.props.author}</h2>
       </div>
     );
+  }
+}
+
+type PostHandler = (content: string, author: string) => void;
+
+interface PostFormProps {
+  createPostHandler: PostHandler;
+}
+
+interface PostFormState {
+  content: string;
+  author: string;
+}
+
+class PostForm extends Component<PostFormProps, PostFormState> {
+  constructor(props: PostFormProps) {
+    super(props);
+    this.state = {
+      content: '',
+      author: '',
+    };
+  }
+
+  submitPost(event: FormEvent<HTMLFormElement>) {
+    event.stopPropagation();
+    event.preventDefault();
+    this.props.createPostHandler(this.state.content, this.state.author);
+    this.setState({
+      content: '',
+      author: '',
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.submitPost(event)}>
+        <div>
+          <label>Content</label>
+          <textarea
+            value={this.state.content}
+            onChange={(event) => {this.setContent(event)}}
+          >
+          </textarea>
+        </div>
+
+        <div>
+          <label>Author</label>
+          <input
+            type="text"
+            value={this.state.author}
+            onChange={(event) => {this.setAuthor(event)}}
+          />
+        </div>
+
+        <button type="submit">Create Post</button>
+      </form>
+    );
+  }
+
+  private setContent(event: ChangeEvent<HTMLTextAreaElement>) {
+    this.setState({
+      content: event.target.value,
+      author: this.state.author,
+    });
+  }
+
+  private setAuthor(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      content: this.state.content,
+      author: event.target.value,
+    });
   }
 }
